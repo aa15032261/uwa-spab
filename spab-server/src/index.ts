caa48cd63d575f7b6ae3dbff58c7bc7ea97fb5cf
Tom Chan <aa15032261@gmail.com>
2021-03-04 03:13:12 +0800

    test

--- spab-server/src/index.ts
+++ spab-server/src/index.ts
@@ -12,7 +12,7 @@ import { RestApi } from './RestApi';
 import { Session } from 'express-session';
 
 function main() {
-    const app = expres();
+    const app = express();
     const httpServer = http.createServer(app);
 
     new MongoDbConnection(
