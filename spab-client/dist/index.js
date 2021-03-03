9bf043b271b99b2f571d8e6b76b90deecde3b4ac
Tom Chan <aa15032261@gmail.com>
2021-03-04 03:26:36 +0800

    updated gitignore

--- spab-client/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const MainController_1 = require("./MainController");
-// allow unsafe tls connection
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-new MainController_1.MainController();
