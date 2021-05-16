c9b27a752cb67217ee26e9d8309f718b9ce9c59a
Tom Chan <aa15032261@gmail.com>
2021-05-16 11:57:36 +0800

    added comments to Config.ts

--- spab-passthrough/src/Config.ts
+++ spab-passthrough/src/Config.ts
@@ -1,10 +1,17 @@
 declare global {
+    /** Development mode - currently not in used */
     var DEV: boolean;
+
+    /** Local tcp port */
     var LOCAL_PORT: number;
 
+    /** Server url */
     var SERVER_URL: string;
+    /** Passthrough WebSocket API path */
     var PASSTHROUGH_WSAPI_PATH: string;
+    /** Client's API token */
     var CLIENT_API_TOKEN: string;
+    /** Client's two-factor login settings */
     var CLIENT_TWO_FACTOR: {type: string, secret: string};
 }
 
