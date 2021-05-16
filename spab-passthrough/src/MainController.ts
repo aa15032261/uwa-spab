6666c9ee14062fd66ed38286d73da92486b41ede
Tom Chan <aa15032261@gmail.com>
2021-05-16 11:44:25 +0800

    added comments to MainController.ts

--- spab-passthrough/src/MainController.ts
+++ spab-passthrough/src/MainController.ts
@@ -11,8 +11,11 @@ export class MainController {
     private _socket: Socket;
     private _localSockets = new Set<net.Socket>();
 
+    /**
+     * MainController handles mavlink data passthrough
+     */
     constructor() {
-        // init socket io
+        // Initialise the passthrough socket
         this._socket = io(SERVER_URL, {
             path: PASSTHROUGH_WSAPI_PATH,
             rejectUnauthorized: false,
@@ -33,9 +36,11 @@ export class MainController {
             }
         });
 
+        // Creates a local tcp socket
         let server = net.createServer((socket) => {
             this._localSockets.add(socket);
 
+            // send all data to the passthrough socket
             socket.on('data', (data: Buffer) => {
                 if (this._checkOnline()) {
                     this._socket.emit('rawData', data);
@@ -46,10 +51,9 @@ export class MainController {
                 this._localSockets.delete(socket);
             });
         });
-
         server.on('error', (e) => { });
-
         server.listen(LOCAL_PORT, '127.0.0.1');
+
 
         this._socket.on('connect', () => {
             this._connectHandler();
@@ -61,14 +65,15 @@ export class MainController {
             this._disconnectHandler(err);
         });
         this._socket.on('disconnect', (reason: string) => {
+            // Reconnects in 30 seconds if kicked by the server
             this._disconnectHandler(reason);
 
-            // reconnect in 30 seconds if kicked by the server
             setTimeout(() => {
                 this._socket.connect();
             }, 30000);
         });
 
+        // Sends data to local tcp socket
         this._socket.on('rawData', (data: Buffer) => {
             for (let socket of this._localSockets) {
                 socket.write(data);
@@ -85,6 +90,11 @@ export class MainController {
         console.log('server disconnected');
     };
 
+
+    /**
+     * Checks if the passthrough socket is connected to the server
+     * @returns {boolean} - True if the socket is connected to the server, otherwise, false
+     */
     private _checkOnline(): boolean {
         if (this._socket.connected) {
             return true;
